d0cb2b43e08e6705b82f1f2a5cc97ab3a899a0e0
Suyash Sunil Patalbansi <89703368+Suyash03022003@users.noreply.github.com>
2022-10-27 16:48:05 +0530

    Index file added

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function loaded() {
+    var ele = document.getElementById("links");
+    ele.style.width = "100%";
+    ele.style.backgroundColor = "#0191C6";
+    var card1 = document.getElementById("card1");
+    var card2 = document.getElementById("card2");
+    var card3 = document.getElementById("card3");
+    var card4 = document.getElementById("card4");
+    card1.style.marginRight = "20px";
+    card2.style.marginLeft = "20px";
+    card3.style.marginRight = "20px";
+    card4.style.marginLeft = "20px";
+    var h = document.getElementById("head");
+    h.style.marginLeft = "0%";
+    h.style.color= "black";
+}
