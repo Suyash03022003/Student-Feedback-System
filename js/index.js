a83690f54dae850e8f0043df17641e8ca93f1e72
Suyash Sunil Patalbansi <89703368+Suyash03022003@users.noreply.github.com>
2022-12-02 11:14:57 +0530

    final index

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 function load() {
-    let ss = document.getElementById("carousel");
-    ss.style.margin = "0";
+    var divimg = document.getElementById("image");
+    divimg.style.margin = "0";
+    divimg.style.opacity = "1";
 }
 
 function loaded() {
     let length = 100;
-    let length1 = 150;
-    let length2 = 300;
+    let length1 = 320;
+    let length2 = 500;
     let length3 = 700;
-    let length4 = 850;
+    let length4 = 900;
     var img = document.getElementById("img");
     var h = document.getElementById("head");
     var card1 = document.getElementById("card1");
@@ -20,7 +21,6 @@ function loaded() {
     var l = document.getElementById("links");
     var link = document.getElementsByClassName("link");
     var heading = document.getElementById("heading");
-    var carousel = document.getElementById("carousel");
     if (pageYOffset >= length) {
         img.style.height = "95%";
         ele.style.backgroundColor = "#102543";
@@ -31,7 +31,6 @@ function loaded() {
         link[0].style.color = "white";
         link[1].style.color = "white";
         link[2].style.color = "white";
-        carousel.style.paddingBottom = "100px";
     }
     else {
         img.style.height = "100%";
@@ -42,38 +41,48 @@ function loaded() {
         l.style.height = "100px";
         link[0].style.color = "#102543";
         link[1].style.color = "#102543";
-        link[2].style.color = "#102543";   
-        // var str = "<div class='img'><img id='img' src='images/bitlogo.jpg' alt='BIT Logo'></div><h2 id='heading'>Student Feedback System</h2><div class='links' id='links'><a href='#' class='active link'>Home</a><a href='#' class='link'>About Us</a><a href='#' class='link'>Contact Us</a></div>";          
-        // ele.innerHTML = str;
+        link[2].style.color = "#102543";
     }
     if (pageYOffset >= length1) {
         h.style.marginLeft = "0%";
+        h.style.fontSize = "35px";
         h.style.color = "#102543";
     }
     else {
         h.style.marginLeft = "90%";
         h.style.color = "#E4EDF9";
+        h.style.fontSize = "10px";
     }
     if (pageYOffset >= length2) {
         card1.style.marginRight = "20px";
         card2.style.marginLeft = "20px";
+        card1.style.opacity = "1";
+        card2.style.opacity = "1";
     }
     else {
         card1.style.marginRight = "200px";
         card2.style.marginLeft = "200px";
+        card1.style.opacity = "0";
+        card2.style.opacity = "0";
     }
     if (pageYOffset >= length3) {
         card3.style.marginRight = "20px";
         card4.style.marginLeft = "20px";
+        card3.style.opacity = "1";
+        card4.style.opacity = "1";
     }
     else {
         card3.style.marginRight = "200px";
         card4.style.marginLeft = "200px";
+        card3.style.opacity = "0";
+        card4.style.opacity = "0";
     }
     if (pageYOffset >= length4) {
         card5.style.marginTop = "20px";
+        card5.style.opacity = "1";
     }
     else {
         card5.style.marginTop = "200px";
+        card5.style.opacity = "0";
     }
 }
