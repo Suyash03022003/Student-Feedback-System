514df701e7dfaed2b1391bd46a94d41feb5bc854
Suyash Sunil Patalbansi <89703368+Suyash03022003@users.noreply.github.com>
2022-12-02 09:13:34 +0530

    updated index

--- js/index.js
+++ js/index.js
@@ -77,23 +77,3 @@ function loaded() {
         card5.style.marginTop = "200px";
     }
 }
-
-let slideIndex = 1;
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-plusSlides(slideIndex);
-setInterval(plusSlides, 3000, slideIndex);
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
-}
